03c9108d9c823686122cca84d3c1950e6217e1ae
Niels van Hoorn <niels@zekerwaar.nl>
2012-12-19 23:48:57 +0100

    Adding code to also get the second translation on a single line

--- check_translations.sh
+++ check_translations.sh
@@ -14,6 +14,9 @@ TRANSLATED_KEYS_FILE=`mktemp "$TEMP_DIR/translated_keys.XXXXX"`
 DIFF_FILE=`mktemp "$TEMP_DIR/diff_keys.XXXXX"`
 
 grep -Ir "NSLocalizedStringFromTable(@" "$SOURCE_ROOT/$CODE_DIR" | sed -E 's/.*NSLocalizedStringFromTable\(@"(([^"]|\\\")+).*/\1/g' |  sort | uniq > "$USED_KEYS_FILE"
+#Catching the second localized string on the same line
+grep -Ir "NSLocalizedStringFromTable(@" "$SOURCE_ROOT/$CODE_DIR" | sed -E 's/.*NSLocalizedStringFromTable\(@"(([^"]|\\\")+).*NSLocalizedStringFromTable.*/\1/g' | grep -v 'NSLocalizedStringFromTable' | sort | uniq >> "$USED_KEYS_FILE"
+cat $USED_KEYS_FILE | sort | uniq > "$USED_KEYS_FILE"
 grep "[^\[]\[[^\[]" "$SOURCE_ROOT/$CODE_DIR/$LANGUAGE_FILE" | sed -E 's/.*\[(.*)\].*/\1/' | sort | uniq > "$TRANSLATED_KEYS_FILE"
 
 diff -u "$USED_KEYS_FILE" "$TRANSLATED_KEYS_FILE" > "$DIFF_FILE"
